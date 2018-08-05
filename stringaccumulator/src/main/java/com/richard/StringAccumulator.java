9ab038b89c6337f2c78f885e82a46a384c9b0731
Richard Chan <richardchankiyin@gmail.com>
2018-08-05 09:43:17 +0800

    init stringaccumulator

---
+++ stringaccumulator/src/main/java/com/richard/StringAccumulator.java
@@ -0,0 +1,14 @@
+package com.richard;
+
+/**
+ * This is the class to support
+ * parsing of string and sum of
+ * integer
+ */
+public class StringAccumulator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "To be implemented!" );
+    }
+}
---
+++ stringaccumulator/src/test/java/com/richard/StringAccumulatorTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringAccumulatorTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+}
