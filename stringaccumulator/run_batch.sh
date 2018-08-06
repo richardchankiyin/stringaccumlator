935eeb7094a33dcdd5ca52605c14eccde1ab17c7
Richard Chan <richardchankiyin@gmail.com>
2018-08-06 19:54:32 +0800

    add run_batch.sh and modify gitignore

--- stringaccumulator/run_batch.sh
+++ stringaccumulator/run_batch.sh
@@ -3,4 +3,9 @@
 args="$1"
 echo "args--$args"
 jar="target/stringaccumulator-jar-with-dependencies.jar"
-java -cp $jar com.richard.StringAccumulator "$args"
+jvmargs="-Xloggc:log/`date +%F_%H-%M-%S`-gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintTenuringDistribution -XX:+PrintGCCause
+-XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=5M"
+
+if [ ! -d "log" ]; then mkdir log; fi
+
+java -cp $jar $jvmargs com.richard.StringAccumulator "$args"
